8b6ea6d5ba274319c1fe9bd5bd85c8d731318514
nomura kouji <nomurakouji52@gmail.com>
2022-02-05 23:28:14 +0900

    画像アップロード機能

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,12 +3,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
   
-  ###### 必要か不明 
   include CarrierWave::MiniMagick
+  process resize_to_fit: [300, 200]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
-  process resize_to_limit: [50, 50] 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
