9b277b78b4ce84cb136be4df0b185cb4166ce5c6
nomura kouji <nomurakouji52@gmail.com>
2022-02-07 09:59:51 +0900

    マイグレーションファイルのnofile修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_124735) do
+ActiveRecord::Schema.define(version: 2022_02_07_004633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assigns", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.bigint "assign_id"
+    t.index ["assign_id"], name: "index_posts_on_assign_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -43,5 +56,6 @@ ActiveRecord::Schema.define(version: 2022_02_04_124735) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "assigns"
   add_foreign_key "posts", "users"
 end
