c938a69ac3bf6a28502a83cea205463278b1c33e
nomura kouji <nomurakouji52@gmail.com>
2022-02-07 12:40:42 +0900

    departmentテーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  belongs_to :department
   mount_uploader :image, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   post '/homes/guest_admin_sign_in', to: 'homes#new_guestadmin'
   resources :posts
   resources :users, :only => [:index, :edit, :show, :update]
+  resources :departments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
