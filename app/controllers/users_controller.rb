e43a7fc1d0c6e083b34c88294195466a63a632ec
nomura kouji <nomurakouji52@gmail.com>
2022-02-08 15:52:12 +0900

    ユーザー一覧の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
   def index
     @users = User.all
+    @departments = Department.all
   end
 
   def edit
