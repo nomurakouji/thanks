aaf05bf37ae74004cf2a98e3d14ca06b35aac3a4
nomura kouji <nomurakouji52@gmail.com>
2022-02-08 14:46:49 +0900

    ゲストユーザーに部門ID付与

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,7 @@ class HomesController < ApplicationController
   def new_guest
     user = User.find_or_create_by(email: 'guest@guest.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.department_id = 6
     end
   # 上記のuserをログインさせることができる(deviseのメソッド)
   sign_in user
@@ -12,6 +13,7 @@ class HomesController < ApplicationController
     guestadmin = User.find_or_create_by(email: 'guestadmin@guestadmin.com') do |guestadmin|
       guestadmin.password = SecureRandom.urlsafe_base64
       guestadmin.admin = true
+      guestadmin.department_id = 6
     end
   # 上記のguestadminをログインさせることができる(deviseのメソッド)
   sign_in guestadmin
