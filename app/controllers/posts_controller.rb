f6e789e1d44b869dd3478eaabdd35d2072bdfb08
nomura kouji <nomurakouji52@gmail.com>
2022-02-15 14:41:02 +0900

    画像サイズをレスポンシブに

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,9 @@ class PostsController < ApplicationController
     @newcomings = Post.all.order(created_at: :desc) 
     # いいね数ランキング
     @likes_rankings = Post.find(Like.group(:post_id).order('count(post_id) desc').pluck(:post_id))
-    
+    # フォローしている順
+    @follows = Post.where(user_id: [*current_user.following_ids]).order(created_at: :desc)
+   
     # @follows = Post.find(current_user.followed.group(:followed_id).order('count(post_id) desc').pluck(:followed_id))
 
   end
