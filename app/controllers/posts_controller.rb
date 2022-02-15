99935c589e22b50b4b1c43ac4f1071b6483fbb02
nomura kouji <nomurakouji52@gmail.com>
2022-02-15 11:14:25 +0900

    新着順のスライダー

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all
- 
+    @newcomings = Post.all.order(created_at: :desc) 
+
   end
 
   # GET /posts/1 or /posts/1.json
