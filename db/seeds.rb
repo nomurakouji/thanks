12f406f07e1695f7a1882085874dec8d29aa2980
nomura kouji <nomurakouji52@gmail.com>
2022-02-17 16:47:28 +0900

    seedデータ画像部分修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,41 +17,41 @@ User.create!(department_id: Department.third.id,
             name: 'arisa',
             email: 'arisa@jarisa.com',
             password: 'password',
-            image: open('app/assets/images/pexels-los-muertos-crew-8446867.jpg'),
+            image: open('./app/assets/images/pexels-los-muertos-crew-8446867.jpg'),
             )
 
 User.create!(department_id: Department.third.id,
             name: 'john',
             email: 'john@john.com',
             password: 'password',
-            image: open('app/assets/images/pexels-andrea-piacquadio-3760260.jpg'),
+            image: open('./app/assets/images/pexels-andrea-piacquadio-3760260.jpg'),
             )
 
 User.create!(department_id: Department.third.id,
             name: 'emily',
             email: 'emily@emily.com',
             password: 'password',
-            image: open('app/assets/images/pexels-andrea-piacquadio-3763188.jpg'),
+            image: open('./app/assets/images/pexels-andrea-piacquadio-3763188.jpg'),
             )
 
 User.create!(department_id: Department.last.id,
             name: 'rihana',
             email: 'rihana@rihana.com',
             password: 'password',
-            image: open('app/assets/images/pexels-andrea-piacquadio-3764152.jpg'),
+            image: open('./app/assets/images/pexels-andrea-piacquadio-3764152.jpg'),
             )
 
 User.create!(department_id: Department.last.id,
             name: '北河玲子',
             email: 'kitakawa@reiko.com',
             password: 'password',
-            image: open('app/assets/images/pexels-pixabay-415829.jpg'),
+            image: open('./app/assets/images/pexels-pixabay-415829.jpg'),
             )
 
 User.create!(department_id: Department.last.id,
             name: '坂本龍二',
             email: 'sakamoto@ryuji.com',
             password: 'password',
-            image: open('app/assets/images/pexels-spencer-selover-428364.jpg'),
+            image: open('./app/assets/images/pexels-spencer-selover-428364.jpg'),
             )
 
