fee9f2438cea1b25d28a6f76efe46f56e876934f
nomura kouji <nomurakouji52@gmail.com>
2022-02-17 18:14:01 +0900

    seedデータ画像部分修正4

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Department.create!(:name => "管理者")
-Department.create!(:name => "ゲストログイン")
-Department.create!(:name => "社長室")
-Department.create!(:name => "広報宣伝部")
-Department.create!(:name => "営業部")
+# Department.create!(:name => "管理者")
+# Department.create!(:name => "ゲストログイン")
+# Department.create!(:name => "社長室")
+# Department.create!(:name => "広報宣伝部")
+# Department.create!(:name => "営業部")
 
-User.create!(department_id: Department.third.id,
-            name: 'arisa',
-            email: 'arisa@jarisa.com',
-            password: 'password',
-            image:File.open('./app/assets/images/pexels-los-muertos-crew-8446867.jpg','r').read
-            )
+# User.create!(department_id: Department.third.id,
+#             name: 'arisa',
+#             email: 'arisa@jarisa.com',
+#             password: 'password',
+#             image:File.open('./app/assets/images/pexels-los-muertos-crew-8446867.jpg','r').read
+#             )
 
 User.create!(department_id: Department.third.id,
             name: 'john',
@@ -27,37 +27,37 @@ User.create!(department_id: Department.third.id,
             image:File.open('./app/assets/images/pexels-andrea-piacquadio-3760260.jpg','rb').read
             )
 
-User.create!(department_id: Department.third.id,
-            name: 'emily',
-            email: 'emily@emily.com',
-            password: 'password',
-            image:File.open('./app/assets/images/pexels-andrea-piacquadio-3763188.jpg','b').read
-            )
+# User.create!(department_id: Department.third.id,
+#             name: 'emily',
+#             email: 'emily@emily.com',
+#             password: 'password',
+#             image:File.open('./app/assets/images/pexels-andrea-piacquadio-3763188.jpg','b').read
+#             )
 
-User.create!(department_id: Department.last.id,
-            name: 'rihana',
-            email: 'rihana@rihana.com',
-            password: 'password',
-            image:File.open('./app/assets/images/pexels-andrea-piacquadio-3764152.jpg','r').read
-            )
+# User.create!(department_id: Department.last.id,
+#             name: 'rihana',
+#             email: 'rihana@rihana.com',
+#             password: 'password',
+#             image:File.open('./app/assets/images/pexels-andrea-piacquadio-3764152.jpg','r').read
+#             )
 
-User.create!(department_id: Department.last.id,
-            name: '北河玲子',
-            email: 'kitakawa@reiko.com',
-            password: 'password',
-            image:File.open('./app/assets/images/pexels-pixabay-415829.jpg').read
-            )
+# User.create!(department_id: Department.last.id,
+#             name: '北河玲子',
+#             email: 'kitakawa@reiko.com',
+#             password: 'password',
+#             image:File.open('./app/assets/images/pexels-pixabay-415829.jpg').read
+#             )
 
-User.create!(department_id: Department.last.id,
-            name: '坂本龍二',
-            email: 'sakamoto@ryuji.com',
-            password: 'password',
-            image:File.open('./app/assets/images/pexels-spencer-selover-428364.jpg','rb').read
-            )
+# User.create!(department_id: Department.last.id,
+#             name: '坂本龍二',
+#             email: 'sakamoto@ryuji.com',
+#             password: 'password',
+#             image:File.open('./app/assets/images/pexels-spencer-selover-428364.jpg','rb').read
+#             )
 
-            User.create!(department_id: Department.last.id,
-                name: '坂本さん',
-                email: 'sakamoto@san.com',
-                password: 'password',
-                image:File.open('./app/assets/images/pexels-spencer-selover-428364.jpg','b').read
-                )
+#             User.create!(department_id: Department.last.id,
+#                 name: '坂本さん',
+#                 email: 'sakamoto@san.com',
+#                 password: 'password',
+#                 image:File.open('./app/assets/images/pexels-spencer-selover-428364.jpg','b').read
+#                 )
