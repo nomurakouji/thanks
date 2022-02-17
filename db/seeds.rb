f22b312a06756490c9ae6a16f1d550d3f0fb3464
nomura kouji <nomurakouji52@gmail.com>
2022-02-17 15:00:01 +0900

    seedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,33 +17,40 @@ User.create!(department_id: Department.third.id,
                 name: 'john',
                 email: 'john@john.com',
                 password: 'password',
-                image:File.open('app/assets/images/pexels-andrea-piacquadio-3760260.jpg'),
+                image:File.open('pexels-andrea-piacquadio-3760260.jpg'),
                 )
 
 User.create!(department_id: Department.third.id,
             name: 'emily',
             email: 'emily@emily.com',
             password: 'password',
-            image:File.open('app/assets/images/pexels-andrea-piacquadio-3763188.jpg'),
+            image:File.open('pexels-andrea-piacquadio-3763188.jpg'),
             )
 
 User.create!(department_id: Department.last.id,
             name: 'rihana',
             email: 'rihana@rihana.com',
             password: 'password',
-            image:File.open('app/assets/images/pexels-andrea-piacquadio-3764152.jpg'),
+            image:File.open('pexels-andrea-piacquadio-3764152.jpg'),
             )
 
 User.create!(department_id: Department.last.id,
             name: '北河玲子',
             email: 'kitakawa@reiko.com',
             password: 'password',
-            image:File.open('app/assets/images/pexels-pixabay-415829.jpg'),
+            image:File.open('pexels-pixabay-415829.jpg'),
             )
 
 User.create!(department_id: Department.last.id,
             name: '坂本龍二',
             email: 'sakamoto@ryuji.com',
             password: 'password',
-            image:File.open('app/assets/images/pexels-spencer-selover-428364.jpg'),
+            image:File.open('pexels-spencer-selover-428364.jpg'),
+            )
+
+User.create!(department_id: Department.last.id,
+            name: 'jehcy',
+            email: 'jehcy@jehcy.com',
+            password: 'password',
+            image:File.open('pexels-spencer-selover-428364.jpg'),
             )
