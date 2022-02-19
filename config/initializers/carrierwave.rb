41e48e0dbef64bf81f34730a593be669d8432c09
nomura kouji <nomurakouji52@gmail.com>
2022-02-19 12:33:44 +0900

    carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
         provider:              'AWS',
-        aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        aws_access_key_id:     'AKIA2A3RAKUL3MFEL7O5',
+        aws_secret_access_key: '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',
         region:                'ap-northeast-1',
         path_style:            true,
     }
