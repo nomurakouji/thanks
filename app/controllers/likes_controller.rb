dfb81420237171e64b0eb12d57f6057fcbf6d037
nomura kouji <nomurakouji52@gmail.com>
2022-02-23 11:27:14 +0900

    非同期いいねのテスト2

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,7 +4,6 @@ class LikesController < ApplicationController
     # いいねを通知するメソッド
     # いいねが押された時、つまりlikes#createが要求された時にいいね通知がnotificationsテーブルに登録される必要がある
     # postモデルでインスタンスメソッドを新しく作って、likes#createに記述してこの動きを実装
-    # @post = Post.find(params[:post_id])
     @post.create_notification_by(current_user)
 
     # いいねのlike_idを作成し、Likeモデル経由でuser_idとpost_idをDBに持たせる
@@ -18,8 +17,10 @@ class LikesController < ApplicationController
     Notification.find_by(visiter_id: current_user, post_id: params[:post_id]).destroy
     # redirect_to posts_path
   end
-
+  
+  # いいねの非同期にも使用
   def set_variables
     @post = Post.find(params[:post_id])
+    @follow = Post.where(user_id: [*current_user.following_ids]).order(created_at: :desc)
   end
 end
