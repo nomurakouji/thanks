383a9e926c8849acbd857aca533bd968990aa457
nomura kouji <nomurakouji52@gmail.com>
2022-03-10 11:01:09 +0900

    管理者画面の修正

--- spec/models/chat_spec.rb
+++ spec/models/chat_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Chat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Postモデル機能' do
+    context 'messageにnilがある場合' do
+        it 'バリデーションにかかる' do
+          chat = FactoryBot.build(:chat,message: nil)
+          chat.valid?
+        end    
+    end
+    context 'messageが51文字以上ある場合' do
+      it 'バリデーションにかかる' do
+        chat = FactoryBot.build(:chat,message: "123456789012345678901234567890123456789012345678901234567890")
+        chat.valid?
+      end    
+    end
+  end
 end
